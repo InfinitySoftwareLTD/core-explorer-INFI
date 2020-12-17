1e642af2c89318a0eef3d318e4c181f6c905ea28
Infinity explorer Deployer <support@infinitysoftware.io>
2020-12-17 12:49:05 +0000

    chore: prepare new network config 🎉 Infinity

---
+++ start-explorer.sh
@@ -0,0 +1,6 @@
+NETWORK="$1"
+if [ -z "$NETWORK" ]; then
+    NETWORK="testnet"
+fi
+HOST="159.65.199.136" PORT="4200" yarn build:"$NETWORK"
+EXPLORER_HOST="159.65.199.136" EXPLORER_PORT="4200" pm2 start /home/lionel/core-explorer/express-server.js --name explorer
