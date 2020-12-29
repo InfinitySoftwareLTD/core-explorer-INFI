685dbcc9ded28b1c7c979237b0d603764f593c87
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-12-29 20:24:41 +0400

    Update start-explorer.sh

--- start-explorer.sh
+++ start-explorer.sh
@@ -3,4 +3,4 @@ if [ -z "$NETWORK" ]; then
     NETWORK="mainnet"
 fi
 HOST="142.93.106.212" PORT="4200" yarn build:"$NETWORK"
-EXPLORER_HOST="142.93.106.212" EXPLORER_PORT="4200" pm2 start /home/YOUR_NAME/core-explorer-INFI/express-server.js --name explorer
+EXPLORER_HOST="142.93.106.212" EXPLORER_PORT="4200" pm2 start /home/YOUR_NAME_FILE/core-explorer-INFI/express-server.js --name explorer
