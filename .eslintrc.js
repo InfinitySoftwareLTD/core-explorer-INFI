c08ce7288419e7de6de12ba474e79cb4d4de8f3e
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2021-06-27 10:10:02 +0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
     "no-fallthrough": "off",
     "getter-return": "off",
     "comma-dangle": ["error", "always-multiline"],
-    "allowEmptyCatch": "error",
+    "allowEmptyCatch": "off",
   },
   parserOptions: {
     parser: "@typescript-eslint/parser",
